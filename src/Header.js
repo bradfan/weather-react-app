36940861f71298cd6d80bf246756b67b4dff9247
Brad <brt.jst@gmail.com>
2021-07-28 17:15:10 -0700

    header css

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
     return (
-        <div>
-            
+        <div class="header">
+            <h1 class="header-text">WEATHER AT A GLANCE</h1>
         </div>
     )
 }
