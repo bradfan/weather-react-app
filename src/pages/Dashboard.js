ca35519c423dc11cb62cc61c6f373227527ce0da
Brad <brt.jst@gmail.com>
2021-07-29 15:32:31 -0700

    add input apply css

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React from "react";
+// import { api_key } from "../api.json";
 
 function Dashboard() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <div class="input-box">
+        <input
+          class="input-window"
+          //   value={city}
+          //   onChange={handleInputChange}
+          name="text"
+          className="form-control me-2"
+          type="text"
+          placeholder="Enter City Here"
+          aria-label="Search"
+        />
+        <button 
+        class="btn btn-white btn-animated"
+        type="submit"
+        // onClick={(event) => {
+        //   console.log(event.target.value);
+        //   handleSubmit(event);
+        // }}
+        >
+            Get Weather!!</button>
+
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
