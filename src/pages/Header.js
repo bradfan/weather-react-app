723d5282beceeaca17478d0e6751b8f6d1848b7a
Brad <brt.jst@gmail.com>
2021-07-29 14:47:47 -0700

    add content and adjust css header.js

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div class="header">
             <h1 class="header-title">WEATHER AT A GLANCE</h1>
+            <h3 class="header-text">Your app for up to date weather with a five day forecast</h3>
         </div>
     )
 }
