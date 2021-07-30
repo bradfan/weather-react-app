19a33e046358c680d2ef1209b69007f0b83eed08
Brad <brt.jst@gmail.com>
2021-07-29 18:19:51 -0700

    begin dasboard function

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,31 +1,55 @@
-import React from "react";
-// import { api_key } from "../api.json";
+import React, { useState, useCallback } from "react";
+import { api_key } from "/api.json";
 
 function Dashboard() {
+  const [currentWeather, setCurrentWeather] = useState({});
+  const [city, setCity] = useState("");
+
+  const getWeather = () => {
+    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=imperial`;
+    fetch(weatherURL)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("weather:", data);
+        setCurrentWeather(data);
+      });
+  };
+  const handleInputChange = (event) => {
+    const val = event.target.value;
+    setCity(val);
+    console.log("city data:", city);
+  };
+
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+    console.log("data:", currentWeather);
+    getWeather();
+  });
+
   return (
     <div>
       <div class="input-box">
         <input
           className="input-window"
-          //   value={city}
-          //   onChange={handleInputChange}
+          value={city}
+          onChange={handleInputChange}
           name="text"
-        
           type="text"
           placeholder="Enter City"
           aria-label="Search"
         />
-        
-        <button 
-        class="btn btn-white btn-animated"
-        type="submit"
-        // onClick={(event) => {
-        //   console.log(event.target.value);
-        //   handleSubmit(event);
-        // }}
-        >
-            Get Weather!!</button>
 
+        <button
+          class="btn btn-white btn-animated"
+          type="submit"
+          value={city}
+          onClick={(event) => {
+            console.log(event.target.value);
+            onSubmit(event);
+          }}
+        >
+          Get Weather!!
+        </button>
       </div>
     </div>
   );
